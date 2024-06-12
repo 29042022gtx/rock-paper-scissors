3c4f5b3f64b8257524a64f221d7aaf29749b0e0e
Owen <150531389+29042022gtx@users.noreply.github.com>
2024-06-12 22:39:22 +0700

    Write playRound function

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+let humanScore = 0
+let computerScore = 0
 
 
+function playRound(computerChoice, humanChoice) {
+    computerChoice = computerChoice[0].toUpperCase() + computerChoice.slice(1)
+    humanChoice = humanChoice[0].toUpperCase() + humanChoice.slice(1)
+    if (computerChoice == humanChoice)
+        return 'Tie! ' + humanChoice + ' equals to ' + computerChoice
+    else if ((computerChoice == 'Rock'
+            && humanChoice == 'Paper')
+        || (computerChoice == 'Paper'
+            && humanChoice == 'Scissors')
+        ||(computerChoice == 'Scissors'
+            && humanChoice == 'Rock')) {
+        humanScore++
+        return 'You win! ' + humanChoice + ' beats ' + computerChoice
+    }
+    computerScore++    
+    return 'Computer win! ' + computerChoice + ' beats ' + humanChoice
+}
 
 function getHumanChoice() {
     let choice = prompt('1: rock 2: paper 3: scissors')
