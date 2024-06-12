eec5a6d09f3f68155b1dd9555fb79d0c7bff66b9
Owen <150531389+29042022gtx@users.noreply.github.com>
2024-06-12 22:15:23 +0700

    Write getHumanChoice function

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 
 
 
+function getHumanChoice() {
+    let choice = prompt('1: rock 2: paper 3: scissors')
+    choice = parseInt(choice)
+    if (choice == 3)
+        return 'scissors'
+    else if (choice == 2)
+        return 'paper'
+    else
+        return 'rock'
+}
+
 function getComputerChoice() {
     let ranNum = Math.floor(Math.random() * 3)
     if (ranNum == 0)
