7c9d70ff6142e17c7b54d9f8feb680de6f19253d
Owen <150531389+29042022gtx@users.noreply.github.com>
2024-06-12 22:50:40 +0700

    Write playGame function

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
-let humanScore = 0
-let computerScore = 0
+playGame()
 
 
-function playRound(computerChoice, humanChoice) {
-    computerChoice = computerChoice[0].toUpperCase() + computerChoice.slice(1)
-    humanChoice = humanChoice[0].toUpperCase() + humanChoice.slice(1)
-    if (computerChoice == humanChoice)
-        return 'Tie! ' + humanChoice + ' equals to ' + computerChoice
-    else if ((computerChoice == 'Rock'
-            && humanChoice == 'Paper')
-        || (computerChoice == 'Paper'
-            && humanChoice == 'Scissors')
-        ||(computerChoice == 'Scissors'
-            && humanChoice == 'Rock')) {
-        humanScore++
-        return 'You win! ' + humanChoice + ' beats ' + computerChoice
+function playGame() {
+    let humanScore = 0
+    let computerScore = 0
+
+    for (let i = 0; i < 5; i++)
+        console.log(playRound(getComputerChoice(), getHumanChoice()))
+    let res = ''
+    if (humanScore > computerScore)
+        res = 'You are the winner! ' + humanScore + ' - ' + computerScore
+    else if (computerScore > humanScore)
+        res = 'The computer is the winner! ' + computerScore + ' - ' + humanScore
+    else
+        res = 'You tie ' + humanScore + ' - ' + computerScore + ' the computer!'
+    console.log(res)
+
+
+    function playRound(computerChoice, humanChoice) {
+        computerChoice = computerChoice[0].toUpperCase() + computerChoice.slice(1)
+        humanChoice = humanChoice[0].toUpperCase() + humanChoice.slice(1)
+        if (computerChoice == humanChoice)
+            return 'Tie! ' + humanChoice + ' equals to ' + computerChoice
+        else if ((computerChoice == 'Rock'
+                && humanChoice == 'Paper')
+            || (computerChoice == 'Paper'
+                && humanChoice == 'Scissors')
+            ||(computerChoice == 'Scissors'
+                && humanChoice == 'Rock')) {
+            humanScore++
+            return 'You win! ' + humanChoice + ' beats ' + computerChoice
+        }
+        computerScore++    
+        return 'The computer win! ' + computerChoice + ' beats ' + humanChoice
     }
-    computerScore++    
-    return 'Computer win! ' + computerChoice + ' beats ' + humanChoice
 }
 
 function getHumanChoice() {
