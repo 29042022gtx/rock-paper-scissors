6d7e266e053ce6c921042e41edd907e18eaae9f6
Owen <150531389+29042022gtx@users.noreply.github.com>
2024-06-19 13:53:59 +0700

    Add function to buttons

--- script.js
+++ script.js
@@ -5,10 +5,70 @@ const results = document.querySelector('.results')
 const buttons = document.querySelector('.buttons')
 buttons.onclick = clickBtn
 
-
-
 function clickBtn(event) {
-
+  let computerChoice = getComputerChoice()
+  let humanChoice = event.target.id
+  let tr = document.createElement('tr')
+  let td1 = document.createElement('td')
+  td1.textContent = humanChoice
+  let td2 = document.createElement('td')
+  td2.textContent = playRound(computerChoice, humanChoice)
+  let td3 = document.createElement('td')
+  td3.textContent = computerChoice
+  tr.appendChild(td1)
+  tr.appendChild(td2)
+  tr.appendChild(td3)
+  results.appendChild(tr)
+  if (humanScore >= 5 || computerScore >= 5) {
+    tr = document.createElement('tr')
+    let td = document.createElement('td')
+    td.textContent = 'Final results'
+    td.colSpan = '3'
+    td.style.textAlign = 'left'
+    tr.appendChild(td)
+    results.appendChild(tr)
+    tr = document.createElement('tr')
+    td = document.createElement('td')
+    td.textContent = 'Human'
+    tr.appendChild(td)
+    results.appendChild(tr)
+    td = document.createElement('td')
+    td.textContent = '|'
+    tr.appendChild(td)
+    results.appendChild(tr)
+    td = document.createElement('td')
+    td.textContent = 'Computer'
+    tr.appendChild(td)
+    results.appendChild(tr)
+    tr = document.createElement('tr')
+    td = document.createElement('td')
+    td.textContent = humanScore
+    tr.appendChild(td)
+    results.appendChild(tr)
+    td = document.createElement('td')
+    let winner = 'Human'
+    if (humanScore > computerScore)
+      td.textContent = '>'
+    else {
+      td.textContent = '<'
+      winner = 'Computer'
+    }
+    tr.appendChild(td)
+    results.appendChild(tr)
+    td = document.createElement('td')
+    td.textContent = computerScore
+    tr.appendChild(td)
+    results.appendChild(tr)
+    tr = document.createElement('tr')
+    td = document.createElement('td')
+    td.textContent = winner + ' is the winner!'
+    td.colSpan = '3'
+    td.style.textAlign = 'left'
+    tr.appendChild(td)
+    results.appendChild(tr)
+    buttons.onclick = null
+    buttons.removeEventListener('click', clickBtn)
+  }
 }
 
 function playRound(computerChoice, humanChoice) {
