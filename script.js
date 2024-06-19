9fae009afc6ad555cda9596071bd61218ab2491c
Owen <150531389+29042022gtx@users.noreply.github.com>
2024-06-19 12:14:42 +0700

    Add event for buttons

--- script.js
+++ script.js
@@ -1,58 +1,86 @@
 // playGame()
+let humanScore = 0
+let computerScore = 0
+const results = document.querySelector('.results')
+const buttons = document.querySelector('.buttons')
+buttons.onclick = clickBtn
 
+
+
+function clickBtn(event) {
+
+}
+
+function playRound(computerChoice, humanChoice) {
+  computerChoice = computerChoice[0].toUpperCase() + computerChoice.slice(1)
+  humanChoice = humanChoice[0].toUpperCase() + humanChoice.slice(1)
+  if (computerChoice == humanChoice)
+      // return 'Tie! ' + humanChoice + ' equals to ' + computerChoice
+    return '='
+  else if ((computerChoice == 'Rock' && humanChoice == 'Paper')
+      || (computerChoice == 'Paper' && humanChoice == 'Scissors')
+      ||(computerChoice == 'Scissors' && humanChoice == 'Rock')) {
+    humanScore++
+    // return 'You win! ' + humanChoice + ' beats ' + computerChoice
+    return '>'
+  }
+  computerScore++    
+  // return 'The computer win! ' + computerChoice + ' beats ' + humanChoice
+  return '<'
+}
 
 function playGame() {
-    let humanScore = 0
-    let computerScore = 0
+  let humanScore = 0
+  let computerScore = 0
 
-    for (let i = 0; i < 5; i++)
-        console.log(playRound(getComputerChoice(), getHumanChoice()))
-    let res = ''
-    if (humanScore > computerScore)
-        res = 'You are the winner! ' + humanScore + ' - ' + computerScore
-    else if (computerScore > humanScore)
-        res = 'The computer is the winner! ' + computerScore + ' - ' + humanScore
-    else
-        res = 'You tie ' + humanScore + ' - ' + computerScore + ' the computer!'
-    console.log(res)
+  for (let i = 0; i < 5; i++)
+    console.log(playRound(getComputerChoice(), getHumanChoice()))
+  let res = ''
+  if (humanScore > computerScore)
+    res = 'You are the winner! ' + humanScore + ' - ' + computerScore
+  else if (computerScore > humanScore)
+      res = 'The computer is the winner! ' + computerScore + ' - ' + humanScore
+  else
+      res = 'You tie ' + humanScore + ' - ' + computerScore + ' the computer!'
+  console.log(res)
 
 
-    function playRound(computerChoice, humanChoice) {
-        computerChoice = computerChoice[0].toUpperCase() + computerChoice.slice(1)
-        humanChoice = humanChoice[0].toUpperCase() + humanChoice.slice(1)
-        if (computerChoice == humanChoice)
-            return 'Tie! ' + humanChoice + ' equals to ' + computerChoice
-        else if ((computerChoice == 'Rock'
-                && humanChoice == 'Paper')
-            || (computerChoice == 'Paper'
-                && humanChoice == 'Scissors')
-            ||(computerChoice == 'Scissors'
-                && humanChoice == 'Rock')) {
-            humanScore++
-            return 'You win! ' + humanChoice + ' beats ' + computerChoice
-        }
-        computerScore++    
-        return 'The computer win! ' + computerChoice + ' beats ' + humanChoice
+  function playRound(computerChoice, humanChoice) {
+    computerChoice = computerChoice[0].toUpperCase() + computerChoice.slice(1)
+    humanChoice = humanChoice[0].toUpperCase() + humanChoice.slice(1)
+    if (computerChoice == humanChoice)
+        return 'Tie! ' + humanChoice + ' equals to ' + computerChoice
+    else if ((computerChoice == 'Rock'
+            && humanChoice == 'Paper')
+        || (computerChoice == 'Paper'
+            && humanChoice == 'Scissors')
+        ||(computerChoice == 'Scissors'
+            && humanChoice == 'Rock')) {
+        humanScore++
+        return 'You win! ' + humanChoice + ' beats ' + computerChoice
     }
+    computerScore++    
+    return 'The computer win! ' + computerChoice + ' beats ' + humanChoice
+  }
 }
 
 function getHumanChoice() {
-    let choice = prompt('1: rock 2: paper 3: scissors')
-    choice = parseInt(choice)
-    if (choice == 3)
-        return 'scissors'
-    else if (choice == 2)
-        return 'paper'
-    else
-        return 'rock'
+  let choice = prompt('1: rock 2: paper 3: scissors')
+  choice = parseInt(choice)
+  if (choice == 3)
+    return 'scissors'
+  else if (choice == 2)
+    return 'paper'
+  else
+    return 'rock'
 }
 
 function getComputerChoice() {
-    let ranNum = Math.floor(Math.random() * 3)
-    if (ranNum == 0)
-        return 'rock'
-    else if (ranNum == 1)
-        return 'paper'
-    else
-        return 'scissors'
+  let ranNum = Math.floor(Math.random() * 3)
+  if (ranNum == 0)
+    return 'rock'
+  else if (ranNum == 1)
+    return 'paper'
+  else
+    return 'scissors'
 }
